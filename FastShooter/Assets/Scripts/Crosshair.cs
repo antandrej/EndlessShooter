9136dba455bdd2f47e0608d99136b4997532e54e
Andrej <antandrej2003@gmail.com>
2023-02-15 23:54:10 +0100

    Update Crosshair.cs

--- FastShooter/Assets/Scripts/Crosshair.cs
+++ FastShooter/Assets/Scripts/Crosshair.cs
@@ -8,38 +8,39 @@ public class Crosshair : MonoBehaviour
     public Transform shootPoint;
 
     public float shootSpeed = 10f;
-    public float timeToShoot = 1.3f;
-    float originalTime;
+    //public float timeToShoot = 1.3f;
+    //float originalTime;
 
     void Start()
     {
         Cursor.visible = false;
-        originalTime = timeToShoot;
+        //originalTime = timeToShoot;
     }
 
     void Update()
     {
         this.transform.position = Input.mousePosition;
-
+        shootPoint.LookAt(this.transform);
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                if (timeToShoot < 0)
-                {
+                //if (timeToShoot < 0)
+                //{
                     //Debug.Log(hit.point);
                     GameObject currentBullet = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+                    Debug.Log("metak");
                     Rigidbody rb = currentBullet.GetComponent<Rigidbody>();
                     Destroy(currentBullet, 1.25f);
                     rb.AddForce(hit.point * shootSpeed, ForceMode.VelocityChange);
-                    timeToShoot = originalTime;
-                }
+                    //timeToShoot = originalTime;
+                //}
                 /*
                 if (hit.transform.gameObject.tag == "Enemy")
                 {
-                    //hit.transform.gameObject.GetComponent<EnemyController>().health -= Random.Range(30, 40);  --- U BULLET CONTROLLERU JE
+                    //hit.transform.gameObject.GetComponent<EnemyController>().health -= Random.Range(30, 40);  --- U BULLET CONTROLLERU JE ~~~ VRATI OVDE
                 }
                 */
             }
