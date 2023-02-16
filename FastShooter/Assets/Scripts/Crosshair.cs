0bd6806f8d6da67811956d66a1ad148f7ff7a0ae
Andrej <antandrej2003@gmail.com>
2023-02-16 01:19:26 +0100

    Trying Player Shooting

--- FastShooter/Assets/Scripts/Crosshair.cs
+++ FastShooter/Assets/Scripts/Crosshair.cs
@@ -11,6 +11,9 @@ public class Crosshair : MonoBehaviour
     //public float timeToShoot = 1.3f;
     //float originalTime;
 
+    Vector3 mousePos;
+    Vector3 aim;
+
     void Start()
     {
         Cursor.visible = false;
@@ -20,30 +23,41 @@ public class Crosshair : MonoBehaviour
     void Update()
     {
         this.transform.position = Input.mousePosition;
-        shootPoint.LookAt(this.transform);
+
+        //shootPoint.transform.LookAt(Input.mousePosition);
+        //pointingTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.back * Camera.main.transform.position.z);
+        //shootPoint.transform.LookAt(pointingTarget, Vector3.back);
+        //Debug.DrawLine(shootPoint.transform.position, this.transform.position, Color.red);
+
+        mousePos = Input.mousePosition;
+        aim = Camera.main.ScreenToWorldPoint(mousePos);
+
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
+            //if (Physics.Raycast(ray, out hit))
+            //{
+            //if (timeToShoot < 0)
+            //{
+            //Debug.Log(hit.point);
+            shootPoint.transform.LookAt(aim);
+            GameObject currentBullet = Instantiate(bullet, shootPoint.position, Quaternion.identity);
+            currentBullet.transform.LookAt(aim);
+            Debug.Log("metak");
+            Rigidbody rb = currentBullet.GetComponent<Rigidbody>();
+            Destroy(currentBullet, 1.25f);
+            //rb.AddForce(hit.point * shootSpeed, ForceMode.VelocityChange);
+            rb.AddRelativeForce(-transform.forward * shootSpeed);
+            //timeToShoot = originalTime;
+            //}
+            /*
+            if (hit.transform.gameObject.tag == "Enemy")
             {
-                //if (timeToShoot < 0)
-                //{
-                    //Debug.Log(hit.point);
-                    GameObject currentBullet = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
-                    Debug.Log("metak");
-                    Rigidbody rb = currentBullet.GetComponent<Rigidbody>();
-                    Destroy(currentBullet, 1.25f);
-                    rb.AddForce(hit.point * shootSpeed, ForceMode.VelocityChange);
-                    //timeToShoot = originalTime;
-                //}
-                /*
-                if (hit.transform.gameObject.tag == "Enemy")
-                {
-                    //hit.transform.gameObject.GetComponent<EnemyController>().health -= Random.Range(30, 40);  --- U BULLET CONTROLLERU JE ~~~ VRATI OVDE
-                }
-                */
+                //hit.transform.gameObject.GetComponent<EnemyController>().health -= Random.Range(30, 40);  --- U BULLET CONTROLLERU JE ~~~ VRATI OVDE
             }
+            */
+            //}
         }
     }
 }
